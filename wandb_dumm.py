f953a7b9ee32cf28a499f5cef64656ba90e910bd
batuhan <batuhan.yildirim.tr@gmail.com>
2021-08-15 00:27:57 +0200

    trying wandb

---
+++ wandb_dumm.py
@@ -0,0 +1,10 @@
+import wandb
+wandb.init(project="dinoss_deneme", entity="dino-wsss-kth")
+
+config = wandb.config
+
+config.learning_rate = 0.0005
+for i in range(5):
+    wandb.log({"loss": i})
+
+
