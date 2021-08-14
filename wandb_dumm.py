ebae9ac9f5a2b7531ce6732071ba59583567bee6
batuhan <batuhan.yildirim.tr@gmail.com>
2021-08-15 01:17:45 +0200

    trying wandb

--- wandb_dumm.py
+++ wandb_dumm.py
@@ -1,6 +1,6 @@
 import wandb
 wandb.init(project="dinoss_deneme", entity="dino-wsss-kth")
-
+WANDB_API_KEY = "6e590174d1c8348e7c8d5fa11450551633dd06ef"
 config = wandb.config
 
 config.learning_rate = 0.0005
