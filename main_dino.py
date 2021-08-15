f20f35dd2c0e605c069b0119c20b0fbff2882b85
batuhan <batuhan.yildirim.tr@gmail.com>
2021-08-15 19:07:22 +0200

    trying wandb

--- main_dino.py
+++ main_dino.py
@@ -132,7 +132,7 @@ def get_args_parser():
 
 
 def train_dino(args):
-    wandb.init(project="dinoss_try_2", entity="dino-wsss-kth")
+    wandb.init(project="dinoss_try_3", entity="dino-wsss-kth")
     config = wandb.config
 
     config.learning_rate = args.lr
