6973c0e5e84c9302eb2ab9152d9349e5a5372d9b
batuhan <batuhan.yildirim.tr@gmail.com>
2021-08-28 21:06:32 +0200

    Experimenting

--- train_sb.sh
+++ train_sb.sh
@@ -2,7 +2,7 @@
 #SBATCH --mem 10GB
 #SBATCH --gres gpu:3
 #SBATCH --cpus-per-task 8
-#SBATCH --constraint rivendell
+#SBATCH --constraint shelob
 #SBATCH --time 48:00:00
 #SBATCH --mail-type FAIL
 #SBATCH --mail-user batuhany@kth.se
@@ -20,5 +20,5 @@ python -m torch.distributed.launch main_dino.py --data_path /local_storage/datas
     --num_workers 4 \
     --batch_size_per_gpu 16 \
     --saveckp_freq 5 \
-    --exp_name dinosstry5_lr:0.00005_rivendell
+    --exp_name dinosstry5_lr:0.00005_shelob
     # --sstrain_checkpoint ~/local_storage/dino_chkp/dino_deitsmall8_pretrain_full_checkpoint/archive/data.pkl \
