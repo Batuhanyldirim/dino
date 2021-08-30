53ea71b27913deb7453b96d93037f6ce61b726ae
batuhan <batuhan.yildirim.tr@gmail.com>
2021-08-31 01:06:59 +0200

    Experimenting

--- train_sb.sh
+++ train_sb.sh
@@ -16,11 +16,12 @@ pushd ~/dino
 conda activate dinoss
 
 python -m torch.distributed.launch main_dino.py --data_path /local_storage/datasets/voc2012_for_dino \
-    --output_dir /local_storage/users/batuhany/dino/checkpoints9 \
+    --output_dir /local_storage/users/batuhany/dino/checkpoints10 \
     --num_workers 4 \
     --batch_size_per_gpu 16 \
     --saveckp_freq 5 \
-    --exp_name dinosstry9_lr:0.0003_shelob \
+    --exp_name dinosstry10_lr:0.0005_shelob \
     --lr 0.0003 \
-    --min_lr 1e-6
+    --min_lr 1e-6 \
+    --patch_size 8
     # --sstrain_checkpoint ~/local_storage/dino_chkp/dino_deitsmall8_pretrain_full_checkpoint/archive/data.pkl \
--- visualize_attention.py
+++ visualize_attention.py
@@ -124,9 +124,14 @@ if __name__ == '__main__':
             print(f"Take key {args.checkpoint_key} in provided checkpoint dict")
             state_dict = state_dict[args.checkpoint_key]
         # remove `module.` prefix
-        state_dict = {k.replace("module.", ""): v for k, v in state_dict.items()}
+
+
+        #this part has commented
+        """state_dict = {k.replace("module.", ""): v for k, v in state_dict.items()}
         # remove `backbone.` prefix induced by multicrop wrapper
-        state_dict = {k.replace("backbone.", ""): v for k, v in state_dict.items()}
+        state_dict = {k.replace("backbone.", ""): v for k, v in state_dict.items()}"""
+
+
         msg = model.load_state_dict(state_dict, strict=False)
         print('Pretrained weights found at {} and loaded with msg: {}'.format(args.pretrained_weights, msg))
     else:
